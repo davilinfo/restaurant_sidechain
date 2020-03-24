caab8af4d9100296ec24773effe1102e3537e666
davilinfo <davilinfo@gmail.com>
2020-03-24 19:55:01 -0300

    verifying with lisk version 2

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ configDevnet.components.storage.port = 5435;
 configDevnet.components.storage.database = 'lisk_dev';
 configDevnet.components.storage.host = 'localhost'
 
-configDevnet.modules.http_api.access.whitelist.push('127.0.0.1');
-
 configDevnet.app.genesisConfig.BLOCK_TIME = 5;
 
 const app = new Application(genesisBlockDevnet, configDevnet);
