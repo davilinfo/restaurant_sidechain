f24d4cd5c51413c0bedcea31cd047f4e0f646384
davilinfo <davilinfo@gmail.com>
2020-05-31 14:58:11 -0300

    payment testing

--- web/src/components/FormPayment.js
+++ web/src/components/FormPayment.js
@@ -4,35 +4,36 @@ import '../styles.css';
 
 const cryptography = require('@liskhq/lisk-cryptography');
 
-function FormPayment({onSubmit}){         
-    const [passphrase, setPassphrase] = useState('');   
+function FormPayment({onSubmit}){    
+    const [passphrase, setPassphrase] = useState('');    
 
     async function handleSubmit(e){
         e.preventDefault();
 
-        console.log(passphrase);
         const encryptedPassphrase = cryptography.encryptPassphraseWithPassword(passphrase, 'luxuryRestaurant');
 
-        await onSubmit({
+        await onSubmit({         
             encryptedPassphrase
         });
-
-        setPassphrase('');
+        
+        setPassphrase('');        
     }
 
     return (
         <div id="app">
             <div id="content" align="center">                
-                <form onSubmit={handleSubmit}>
-                    <input type="hidden" id="deliveryaddress" name="deliveryaddress"></input>
+                <form onSubmit={handleSubmit}>                                  
                     <div>
-                        <label>Passphrase</label>
+                        <label>Your Lisk Passphrase</label>
                     </div>
                     <div>
                         <input type="text" id="passphrase" name="passphrase" required onChange={e=> setPassphrase(e.target.value)}/>
-                    </div>                    
+                    </div>
                     <div>
-                        <button type="submit">Payment</button>
+                        <label>copy this passphrase and test above if desired: wagon stock borrow episode laundry kitten salute link globe zero feed marble</label>
+                    </div>
+                    <div>
+                        <button type="submit">Order</button>
                     </div>
                 </form>
             </div>
