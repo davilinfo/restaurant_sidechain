0d310c34af3ea8b7eb405bf263002e9ecb7b6267
davilinfo <davilinfo@gmail.com>
2020-08-17 00:05:43 +0000

    contact updated with published article

--- web/src/components/Contact.js
+++ web/src/components/Contact.js
@@ -18,7 +18,7 @@ function Contact(){
                         <p>Email: dav1@liskrestaurant.com</p>
                         <p>Scientific work: A Strategy for Mitigating Denial of Service Attacks on Nodes with Delegate Account of Lisk Blockchain</p>
                         <p>ACM book title: 2020 The 2nd International Conference on Blockchain Technology (ICBCT'20), March 12--14, 2020, Hilo, HI, USA</p>
-                        <p>ACM digital library DOI: 10.1145/3390566.3391684 (to be published)</p>
+                        <p>ACM digital library DOI: 10.1145/3390566.3391684</p>
                         <div className="clear"></div>
                     </div>
                     
