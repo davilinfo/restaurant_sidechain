d64d9c83fd80cf595de7b7d400e0ebede8837900
Davi <davilinfo@gmail.com>
2020-10-21 23:29:15 -0300

    liskrestaurant custom transaction version

---
+++ dish_commands/balance_transfer.js
@@ -0,0 +1,18 @@
+const transactions = require('@liskhq/lisk-transactions');
+const { getNetworkIdentifier } = require('@liskhq/lisk-cryptography');
+const { APIClient } = require('@liskhq/lisk-api-client');
+const { Mnemonic } = require('@liskhq/lisk-passphrase');
+
+const api = new APIClient(['http://localhost:4000']);
+
+var address = "12155463429267245415L";
+let tx = new transactions.TransferTransaction({    
+    amount: transactions.utils.convertLSKToBeddows('200000'),
+    recipientId: address,
+    timestamp: utils.getTimeFromBlockchainEpoch(new Date())    
+});
+
+tx.sign('wagon stock borrow episode laundry kitten salute link globe zero feed marble');
+
+console.log(tx.stringify());
+process.exit(0);
